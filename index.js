6618eb761483d60db06f2fe26222817aefe58702
Roger Robertson Jr <rjrjr@pm.me>
2020-04-30 14:25:08 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Add your functions here
+const map = (arr, func) => {
+    let result = []
+    for(let el of arr){
+        result.push(func(el))
+    }
+    return result
+}
+
+const reduce = (arr, func, start=0) => {
+    let result = start
+    for (let i = 0; i < arr.length; i++) {
+        result = result + func(arr[i])
+    }
+    return result
+}
